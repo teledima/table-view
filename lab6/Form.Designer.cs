c648b5ac815106511b20ccb14fa9bf309f3b348e
Дмитрий Телегин <teledima00@mail.ru>
2020-03-27 17:36:34 +0500

    Добавьте файлы проекта.

---
+++ lab6/Form.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace lab6
+{
+    partial class FormPresentantion
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.data_grid_view = new System.Windows.Forms.DataGridView();
+            this.button_delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.data_grid_view)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // data_grid_view
+            // 
+            this.data_grid_view.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data_grid_view.Location = new System.Drawing.Point(12, 7);
+            this.data_grid_view.Name = "data_grid_view";
+            this.data_grid_view.RowHeadersWidth = 51;
+            this.data_grid_view.RowTemplate.Height = 24;
+            this.data_grid_view.Size = new System.Drawing.Size(494, 431);
+            this.data_grid_view.TabIndex = 0;
+            this.data_grid_view.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.data_grid_view_CellEndEdit);
+            this.data_grid_view.RowValidating += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.data_grid_view_RowValidating);
+            // 
+            // button_delete
+            // 
+            this.button_delete.Location = new System.Drawing.Point(512, 7);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(276, 41);
+            this.button_delete.TabIndex = 5;
+            this.button_delete.Text = "Delete";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // FormPresentantion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.data_grid_view);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormPresentantion";
+            this.Text = "FormDgvIud";
+            ((System.ComponentModel.ISupportInitialize)(this.data_grid_view)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView data_grid_view;
+        private System.Windows.Forms.Button button_delete;
+    }
+}
+
---
+++ lab6/Form.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace lab6
+{
+    public partial class FormPresentantion : Form
+    {
+        private readonly string connect_string = "Host = localhost; Username = postgres; password = postgres; Database = postgres";
+
+        public FormPresentantion()
+        {
+            InitializeComponent();
+            InitializePresentation();
+        }
+        private void InitializePresentation()
+        {
+            data_grid_view.Rows.Clear();
+            data_grid_view.Columns.Clear();
+            data_grid_view.Columns.Add("actor_name", "actor_name");
+            data_grid_view.Columns.Add("film_name", "film_name");
+            data_grid_view.Columns.Add("honorarium", "honorarium");
+            using (var connect = new NpgsqlConnection(connect_string))
+            {
+                connect.Open();
+                var sCommand = new NpgsqlCommand
+                {
+                    Connection = connect,
+                    CommandText = @"SELECT * from view_cinema"
+                };
+                var reader = sCommand.ExecuteReader();
+                while (reader.Read())
+                    data_grid_view.Rows.Add(reader["actor_name"], reader["film_name"], reader["honorarium"]);
+            }
+            foreach (DataGridViewRow row in data_grid_view.Rows)
+            {
+                if (row.Cells[0].Value != null && row.Cells[1].Value != null && row.Cells[2].Value != null)
+                {
+                    var tag = new List<object>();
+                    foreach (DataGridViewCell cell in row.Cells)
+                        tag.Add(cell.Value);
+                    row.Tag = tag;
+                }
+            }
+        }
+
+        private void data_grid_view_RowValidating(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            var row = data_grid_view.Rows[e.RowIndex];
+            if (data_grid_view.IsCurrentRowDirty)
+            {
+                foreach (DataGridViewCell cell in data_grid_view.Rows[e.RowIndex].Cells)
+                {
+                    if (cell.Value is null) return;
+                }
+                if (!e.Cancel)
+                {
+                    using var connect = new NpgsqlConnection(connect_string);
+                    connect.Open();
+                    using var command = new NpgsqlCommand() { Connection = connect };
+                    command.Parameters.AddWithValue("@actor_name", row.Cells["actor_name"].Value);
+                    command.Parameters.AddWithValue("@film_name", row.Cells["film_name"].Value);
+                    command.Parameters.AddWithValue("@honorarium", int.Parse(row.Cells["honorarium"].Value.ToString()));
+                    if (row.Tag != null)
+                    {
+
+                        command.CommandText = @"UPDATE view_cinema SET actor_name = @actor_name, film_name = @film_name, honorarium = @honorarium
+                                                        where actor_name = @old_actor_name and film_name = @old_film_name";
+                        command.Parameters.AddWithValue("@old_actor_name", ((List<object>)row.Tag)[0]);
+                        command.Parameters.AddWithValue("@old_film_name", ((List<object>)row.Tag)[1]);
+                    }
+                    else
+                    {
+                        command.CommandText = @"INSERT INTO view_cinema(actor_name, film_name, honorarium) VALUES (@actor_name, @film_name, @honorarium)";
+                    }
+                    int check = CheckDuplicates(row.Cells["actor_name"].Value.ToString(), row.Cells["film_name"].Value.ToString(), row.Index);
+                    if (check >= 0)
+                    {
+                        data_grid_view.Rows[check].Cells["honorarium"].Value = int.Parse(data_grid_view.Rows[check].Cells["honorarium"].Value.ToString()) + int.Parse(row.Cells["honorarium"].Value.ToString());
+                        row.SetValues(string.Empty, string.Empty, string.Empty);
+                    }
+                    command.ExecuteNonQuery();
+                    connect.Close();
+                    var dataDict = new List<object>();
+                    foreach (var columnsName in new[] { "actor_name", "film_name", "honorarium" })
+                    {
+                        dataDict.Add(row.Cells[columnsName].Value);
+                    }
+
+                    row.Tag = dataDict;
+
+                    row.ErrorText = string.Empty;
+                }
+            }
+        }
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in data_grid_view.SelectedRows)
+                delete_one_row(row);
+            delete_empty_row();
+        }
+        private void delete_empty_row()
+        {
+            DataGridViewRow row_deleted = null;
+            foreach (DataGridViewRow row in data_grid_view.Rows)
+                if (!row.IsNewRow)
+                    if (row.Cells[0].Value.ToString() == string.Empty && row.Cells[1].Value.ToString() == string.Empty && row.Cells[2].Value.ToString() == string.Empty)
+                        row_deleted = row;
+            if (row_deleted != null)
+                data_grid_view.Rows.Remove(row_deleted);
+        }
+        private void delete_one_row(DataGridViewRow row)
+        {
+            using (var connect = new NpgsqlConnection(connect_string))
+            {
+                connect.Open();
+                using var command_delete = new NpgsqlCommand("delete from view_cinema where actor_name = @actor_name and film_name = @film_name", connect);
+                command_delete.Parameters.AddWithValue("@actor_name", data_grid_view.SelectedRows[0].Cells["actor_name"].Value.ToString());
+                command_delete.Parameters.AddWithValue("@film_name", data_grid_view.SelectedRows[0].Cells["film_name"].Value.ToString());
+                command_delete.ExecuteNonQuery();
+                connect.Close();
+            }
+            data_grid_view.Rows.Remove(row);
+        }
+
+        private void data_grid_view_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            var cell = data_grid_view[e.ColumnIndex, e.RowIndex];
+            var value = cell.Value?.ToString();
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                if (e.ColumnIndex == 0)
+                    cell.ErrorText = "Имя не может быть пустым";
+                else if (e.ColumnIndex == 1)
+                    cell.ErrorText = "Название фильма не может быть пустым";
+            }
+            else if (cell.ColumnIndex == 2)
+            {
+                if (int.TryParse(cell.Value.ToString(), out _))
+                {
+                    if (int.Parse(value) <= 0)
+                    {
+                        cell.ErrorText = "Гонорар не может быть отрицательным или нулевым";
+                    }
+                }
+                else
+                {
+                    cell.ErrorText = "Гонорар не должен содержать символы, отличные от цифр";
+                }
+            }
+            else
+            {
+                cell.ErrorText = string.Empty;
+            }
+            RewriteRowErrorText(e.RowIndex);
+        }
+        private void RewriteRowErrorText(int row_index)
+        {
+            var row = data_grid_view.Rows[row_index];
+            List<string> error_columns = new List<string>();
+            foreach (DataGridViewCell cell in row.Cells)
+                if (cell.Value is null)
+                    error_columns.Add(cell.OwningColumn.HeaderText);
+            if (error_columns.Count > 0)
+            {
+                var builder = new StringBuilder("Данные в столбце/столбцах ");
+                foreach (string error_column in error_columns)
+                    builder.Append(error_column + " ");
+                builder.Append(" не могут быть пустыми");
+                row.ErrorText = builder.ToString();
+            }
+            else
+            {
+                if (data_grid_view.IsCurrentRowDirty)
+                    row.ErrorText = "Эта строка не зафиксирована";
+                else
+                    row.ErrorText = string.Empty;
+            }
+        }
+
+        private int CheckDuplicates(string actor_name, string film_name, int index_no_check)
+        {
+            foreach (DataGridViewRow row in data_grid_view.Rows)
+                if (row.Index != index_no_check && !row.IsNewRow)
+                    if (row.Cells[0].Value.ToString() == actor_name && row.Cells[1].Value.ToString() == film_name)
+                        return row.Index;
+            return -1;
+        }
+
+        private void data_grid_view_RowValidated(object sender, DataGridViewCellEventArgs e)
+        {
+            //DataGridViewRow row_deleted = null;
+            //foreach (DataGridViewRow row in data_grid_view.Rows)
+            //    if (!row.IsNewRow)
+            //        if (row.Cells[0].Value.ToString() == string.Empty && row.Cells[1].Value.ToString() == string.Empty && row.Cells[2].Value.ToString() == string.Empty)
+            //            row_deleted = row;
+            //if (row_deleted != null)
+            //    data_grid_view.Rows.Remove(row_deleted);
+        }
+    }
+}
